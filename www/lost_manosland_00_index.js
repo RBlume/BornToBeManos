2de851a710373688d63fa704f03c721a756fc852
Javier Aguerri <javieraguerrimoreno@gmail.com>
2014-03-28 09:36:59 +0100

    remove comments

--- www/lost_manosland_00_index.js
+++ www/lost_manosland_00_index.js
@@ -10,16 +10,12 @@ $("#id00_page").live('pageinit', function(event){
 		where_clause=false;
 		SQLite_extract_full(table0_name,table0_columns,where_clause,on_success,on_empty,on_error);
 		function on_success(extracted_info_array,extracted_indexes,extracted_info_obj){
-			//alert("success");
-			//alert(extracted_info_array);
 			load_page_time(page05,2);
 		}
 		function on_empty(){
-			//alert("empty");
 			load_page_time(page01,2);
 		}
 		function on_error(){
-			//alert("error");
 			load_page_time(page01,2);
 		}
 	}
