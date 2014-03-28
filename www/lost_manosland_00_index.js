acfc9abf40880133f8b449e14989d1294d9a804e
Javier Aguerri <javieraguerrimoreno@gmail.com>
2014-03-28 10:11:36 +0100

    git ignore and basic changes HTML

--- www/lost_manosland_00_index.js
+++ www/lost_manosland_00_index.js
@@ -10,7 +10,7 @@ $("#id00_page").live('pageinit', function(event){
 		where_clause=false;
 		SQLite_extract_full(table0_name,table0_columns,where_clause,on_success,on_empty,on_error);
 		function on_success(extracted_info_array,extracted_indexes,extracted_info_obj){
-			load_page_time(page05,2);
+			load_page_time(page02,2);
 		}
 		function on_empty(){
 			load_page_time(page01,2);
